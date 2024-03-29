489e1c3dcd47523c1971faebaf667d4d822c70a9
Pavan.Kumar <pnttiwari99@gmail.com>
2024-03-29 11:46:16 +0530

    ep6.1

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,27 @@
 const Card = ({ resturent }) => {
-  const { id, name, cuisines, areaName, cloudinaryImageId, avgRating } =
-    resturent.info;
+  const {
+    id,
+    name,
+    cuisines,
+    areaName,
+    cloudinaryImageId,
+    avgRating,
+    costForTwo,
+  } = resturent.info;
   return (
     <div className="card">
       <img
         className="img_card"
-        src="https://images.pexels.com/photos/1833349/pexels-photo-1833349.jpeg?auto=compress&cs=tinysrgb&w=600"
+        src={`https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING/${cloudinaryImageId}`}
         alt="img-card"
       />
       <div className="card-text">
         <p>{name}</p>
-        <p>{cuisines[0]}</p>
-        <p>{cuisines[1]}</p>
+        <p>
+          {cuisines[0]} ,{cuisines[1]}
+        </p>
+
+        <p>{costForTwo}</p>
         <p>{avgRating}</p>
         <p>{areaName}</p>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,9 @@ import Card from "./Card";
 
 const Main = () => {
   const [listOfResturent, setlistOfRestaurant] = useState([]);
+  const [filterResturent, setFilterResturent] = useState([]);
+
+  const [searchtext, setsearchtext] = useState("");
 
   const topRated = () => {
     const Resturent = listOfResturent.filter((s) => s.info.avgRating > 4.3);
@@ -15,13 +18,24 @@ const Main = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    // console.log(
-    //   "data",
-    //   json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
-    // );
+
     setlistOfRestaurant(
-      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilterResturent(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  const handleSearch = () => {
+    let filterdata = listOfResturent.filter((search) =>
+      search.info.name.toLowerCase().includes(searchtext.toLowerCase())
+    );
+    setFilterResturent(filterdata);
+    setsearchtext("");
   };
   useEffect(() => {
     fetchData();
@@ -30,10 +44,19 @@ const Main = () => {
   console.log(listOfResturent);
   return (
     <>
+      <div className="filter">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchtext}
+          onChange={(e) => setsearchtext(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       <button onClick={topRated}>Top rated resturend</button>
       <div className="main">
-        {listOfResturent.map((resturent) => (
-          <Card key={resturent.id} resturent={resturent} />
+        {filterResturent.map((resturent) => (
+          <Card key={resturent.info.id} resturent={resturent} />
         ))}
       </div>
     </>
